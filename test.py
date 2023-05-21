9c1a75422c0b93613b13ab286659cdb87bc2effa
Shrung <87622226+ShrungDN@users.noreply.github.com>
2023-05-21 20:21:26 +0530

    added model

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ import pickle
 from helper_functions import *
 from train import *
 
-model_path = 'D:\IITM Academic Stuff\Sem 8 Books\CS6910\CS6910-Assignment-3\models\LSTM_True_0.0\\'
+model_path = 'D:\IITM Academic Stuff\Sem 8 Books\CS6910\CS6910-Assignment-3\models\LSTM_False_0.358154296875\\'
 
 with open(model_path+'decoder', 'rb') as file:
     decoder = pickle.load(file)
+    print('Decoder Loaded...')
 with open(model_path+'encoder', 'rb') as file:
     encoder = pickle.load(file)
+    print('Encoder Loaded...')
 with open(model_path+'inp_lang', 'rb') as file:
     input_lang = pickle.load(file)
+    print('Input Lang Loaded...')
 with open(model_path+'out_lang', 'rb') as file:
     output_lang = pickle.load(file)
+    print('Output Lang Loaded...')
 with open(model_path+'test_pairs', 'rb') as file:
     test_pairs = pickle.load(file)
 with open(model_path+'config_loss', 'rb') as file:
@@ -22,4 +26,7 @@ with open(model_path+'config_max_length', 'rb') as file:
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_loss, test_acc, test_attentions = validIters(encoder, decoder, input_lang, output_lang, test_pairs, config_loss, config_max_length, device)
-print(test_loss, test_acc, test_attentions)
+print(test_loss, test_acc)
+if test_attentions is not None:
+        plt.matshow(test_attentions)
+        plt.show()
